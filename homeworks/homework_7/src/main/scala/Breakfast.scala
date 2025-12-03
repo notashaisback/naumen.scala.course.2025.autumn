bd4c3e4d27b62f7476e0fbbca601a6573fe3af57
notashaisback <mnatali7772024@gmail.com>
2025-12-03 17:58:41 +0500

    Update Breakfast.scala

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,41 +1,65 @@
 package ru.dru
 
-import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Clock, Scope, ZIO, durationInt}
+import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertCompletesZIO, assertTrue}
 
 import java.time.LocalDateTime
-import scala.concurrent.TimeoutException
+import java.time.temporal.ChronoUnit
+import zio.internal.stacktracer.Tracer._
+import zio.test.Assertion._
 
-case class SaladInfoTime(tomatoTime: Duration, cucumberTime: Duration)
+object BreakfastTest extends ZIOSpecDefault {
 
-
-object Breakfast extends ZIOAppDefault {
-
-  /**
-   * Функция должна эмулировать приготовление завтрака. Продолжительные операции необходимо эмулировать через ZIO.sleep.
-   * Правила приготовления следующие:
-   *  1. Нобходимо вскипятить воду (время кипячения waterBoilingTime)
-   *  2. Параллельно с этим нужно жарить яичницу eggsFiringTime
-   *  3. Параллельно с этим готовим салат:
-   *    * сначала режим  огурцы
-   *    * после этого режим помидоры
-   *    * после этого добавляем в салат сметану
-   *  4. После того, как закипит вода необходимо заварить чай, время заваривания чая teaBrewingTime
-   *  5. После того, как всё готово, можно завтракать
-   *
-   * @param eggsFiringTime время жарки яичницы
-   * @param waterBoilingTime время кипячения воды
-   * @param saladInfoTime информация о времени для приготовления салата
-   * @param teaBrewingTime время заваривания чая
-   * @return Мапу с информацией о том, когда завершился очередной этап (eggs, water, saladWithSourCream, tea)
-   */
-  def makeBreakfast(eggsFiringTime: Duration,
-                    waterBoilingTime: Duration,
-                    saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+  override def spec: Spec[TestEnvironment with Scope, Any] = suite("Breakfast") (
+    test("test1") {
+      for {
+        now <- ZIO.succeed(LocalDateTime.now())
 
 
+        breakfast <- Breakfast.makeBreakfast(
+          10.seconds,
+          5.seconds,
+          SaladInfoTime(
+            7.seconds,
+            7.seconds
+          ),
+          1.seconds).map(_.transform((_, time) => time.truncatedTo(ChronoUnit.SECONDS))).withClock(Clock.ClockLive)
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
+        correct <- ZIO.succeed(Map(
+          "eggs" -> now.plusSeconds(10),
+          "water" -> now.plusSeconds(5),
+          "saladWithSourCream" -> now.plusSeconds(14),
+          "tea" -> now.plusSeconds(6),
+        ).transform((_, time) => time.truncatedTo(ChronoUnit.SECONDS)))
 
+      } yield assertTrue {
+        breakfast == correct
+      }
+    },
+    test("test2") {
+      for {
+        now <- ZIO.succeed(LocalDateTime.now())
+
+
+        breakfast <- Breakfast.makeBreakfast(
+          5.seconds,
+          5.seconds,
+          SaladInfoTime(
+            5.seconds,
+            5.seconds
+          ),
+          5.seconds).map(_.transform((_, time) => time.truncatedTo(ChronoUnit.SECONDS))).withClock(Clock.ClockLive)
+
+        correct <- ZIO.succeed(Map(
+          "eggs" -> now.plusSeconds(5),
+          "water" -> now.plusSeconds(5),
+          "saladWithSourCream" -> now.plusSeconds(10),
+          "tea" -> now.plusSeconds(10),
+        ).transform((_, time) => time.truncatedTo(ChronoUnit.SECONDS)))
+
+      } yield assertTrue {
+        breakfast == correct
+      }
+    }
+  )
 }
